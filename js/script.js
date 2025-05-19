1ec0bf47180901ca6f197c936f73ee0406442837
kapilkumar-uthayakumar <kapilkumar.uthayakumar@stu.ocsb.ca>
2025-05-19 23:47:09 +0000

    2-01

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Kapilkumar.Uthayakumar All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kapilkumar.Uthayakumar
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+/**
+ * Displays an alert with the message "Hello, World!"
+ */
+function myButtonClicked() {
+  document.getElementById("answer").innerHTML = "Hello, World!"
+}
